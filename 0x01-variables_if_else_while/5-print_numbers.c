9274b7681fd8ffaccbd73708945bbb7f9cf7b28d
Okoro-Oluebube <okorooluebube2000@yahoo.com>
2023-12-18 10:45:03 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main --printing single digits in base 10
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1 ; n <= 9 ; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
