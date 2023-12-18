af6b328a7e8097c1afaa2b3f002e6bc0da4596f9
Okoro-Oluebube <okorooluebube2000@yahoo.com>
2023-12-18 09:37:12 -0800

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main --printing lower case and upper case alphabets with putchar function
+ * Return: always return 0
+ */
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 'a' ; alpha = 'z' ; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	for (alpha = 'A' ; alpha = 'Z' ; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
