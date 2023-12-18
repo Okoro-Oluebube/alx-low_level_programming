ba5e8a00a62a60cccb6474fe1e1c575c8c5d3354
Okoro-Oluebube <okorooluebube2000@yahoo.com>
2023-12-18 14:36:14 -0800

    11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main --entry pt
+ * Return: 0
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		for (y = 0 ; y <= 9 ; y++)
+		{
+			for (z = 0 ; z <= 9 ; z++)
+				if (z < y || y < x || z == y || y == x)
+				{
+					continue;
+				}
+				else
+				{
+					putchar('0' + x);
+					putchar('0' + y);
+					putchar('0' + z);
+					if (y < 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
