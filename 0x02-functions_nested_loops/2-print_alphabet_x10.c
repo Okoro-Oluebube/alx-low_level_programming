618694d9116f0f39794274834e157b3005470dde
Okoro-Oluebube <okorooluebube2000@yahoo.com>
2023-12-20 14:48:00 -0800

    introduction of 10x fxn and 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main --counting the alphabet 10x
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,3 +15,19 @@ void print_alphabet(void)
 	putchar('\n');
 	return;
 }
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+		{
+			putchar(alpha);
+		}
+	}
+	putchar('\n');
+	return;
+}
