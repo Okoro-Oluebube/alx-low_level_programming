c6aff557a860b9939af297021a2a408e35fb6771
Okoro-Oluebube <okorooluebube2000@yahoo.com>
2023-12-21 19:27:11 -0800

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * mul --multiplication of two integers
+ * @a: function parameter
+ * @b: function parameter
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	printf("%d", a * b);
+	return (0);
+}
