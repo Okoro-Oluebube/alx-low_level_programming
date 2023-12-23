37cb7c6abcec614211b2b8e5198423f71ecaf46a
Okoro-Oluebube <okorooluebube2000@yahoo.com>
2023-12-22 20:24:46 -0800

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square --prints square using #
+ * @size: fxn parameter
+ * Return: void
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	for (a = 0 ; a <= size ; a++)
+	{
+		for (b = 0 ; b <= size ; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
