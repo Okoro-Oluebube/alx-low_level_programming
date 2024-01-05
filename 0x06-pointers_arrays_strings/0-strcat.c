20676e825c4cd80babc26933687a8b1f8700fb2e
Okoro-Oluebube <okorooluebube2000@yahoo.com>
2024-01-04 18:13:25 -0800

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ * char *_strcat --concatinating two steings
+ * @dest: fxn parameter
+ * @src: fxn parameter
+ * Return: always return 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (0);
+}
