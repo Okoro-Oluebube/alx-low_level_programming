6a600710388a88966e8f2b52f44b3adce01d3fc0
Okoro-Oluebube <okorooluebube2000@yahoo.com>
2024-01-05 08:05:35 -0800

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+ * _strcmp --xhecking out strcmp fxn
+ * @s1: fxn parameter
+ * @s2: fxn parameter 2
+ * Return: return 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = strcmp(s1, s2);
+
+	return (a);
+}
