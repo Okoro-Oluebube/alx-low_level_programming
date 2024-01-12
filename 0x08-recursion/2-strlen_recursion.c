0df775e7029f785587dbf991d3d7b8f79bf92ef1
Okoro-Oluebube <okorooluebube2000@yahoo.com>
2024-01-11 17:44:15 -0800

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,7 +9,9 @@ int _strlen_recursion(char *s)
 {
 	int i;
 
-	i = strlen(s);
-	_putchar(i);
+	if (*s != '\0')
+	{
+		i = 1 + _strlen_recursion(s + 1);
+	}
 	return (i);
 }
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include <math.h>
+/**
+ * _sqrt_recursion --gives the square root of a number.
+ * @n: input variable.
+ * Return: integer value.
+ */
+int _sqrt_recursion(int n)
+{
+	int x;
+
+	x = sqrt(n);
+	return (x);
+}
