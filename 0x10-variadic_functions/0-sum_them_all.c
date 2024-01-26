ff9f8da02e7f26eaf6d4674094f845076af0da05
Okoro-Oluebube <okorooluebube2000@yahoo.com>
2024-01-25 17:58:35 -0800

    0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+/**
+ * sum_them_all --sum of variables.
+ * @n: function parameter.
+ * Return: an integer.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+	unsigned int i;
+
+	sum = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		va_list(var);
+		va_start(var, n);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(var, int);
+		}
+		va_end(var);
+		return (sum);
+	}
+}
