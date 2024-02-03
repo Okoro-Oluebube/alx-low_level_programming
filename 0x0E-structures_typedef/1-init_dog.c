c65a5875a16e3f95d9b62d65325e277583973932
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-02-03 13:00:20 -0800

    1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include "dog.h"
 /**
  * main --main body.
  * init_dog --intializing struct dog variables.
@@ -8,13 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
-}
-int main(void)
-{
-	(*d).name = *name;
-	(*d).age = age;
-	(*d).owner = *owner;
+		d->name = name;
+		(*d).age = age;
+		(*d).owner = owner;
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,6 +6,7 @@
  *@age: fxn membee
  *@owner: fxn member
  */
+#include <stddef.h>
 struct dog
 {
 	char *name;
@@ -14,5 +15,11 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * void free_dog(dog_t *d);
+ */
+/**
+ * dog_t *new_dog(char *name, float age, char *owner);
+ */
 
 #endif /*DOG_H*/
