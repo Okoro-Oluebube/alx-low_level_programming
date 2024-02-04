dce9110c4f812bbc7c036e073fe7e21ee014e751
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-02-04 05:38:14 -0800

    correction

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -8,5 +8,7 @@
 int main(void)
 {
 	print_strings(", ", 2, "Jay", "Django");
+	print_strings("; ", 4, "You know", "", "nothing", NULL);
+	print_strings(NULL, 4, "You ", "know ", "nothing, ", "Jon Snow");	
 	return (0);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,31 +10,29 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list var;
 	unsigned int i;
-	char *s = va_arg(var, char *);
+	char *s;
 
-	if (separator == NULL)
+	va_start(var, n);
+	for (i = 0; i < n; i++)
 	{
-		printf("%s", s);
-	}
-	else
-	{
-		va_start(var, n);
-		for (i = 0; i < n; i++)
+		s = va_arg(var, char *);
+		if (separator == NULL)
 		{
-			if (s == NULL)
+			printf("%s", s);
+		}
+		else if (s == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", s);
+			if (i < n - 1)
 			{
-				printf("(nil)");
-			}
-			else
-			{
-				printf("%s", s);
-				if (i < n - 1)
-				{
-					printf("%s", separator);
-				}
+				printf("%s", separator);
 			}
 		}
-		va_end(var);
 	}
+	va_end(var);
 	printf("\n");
 }
