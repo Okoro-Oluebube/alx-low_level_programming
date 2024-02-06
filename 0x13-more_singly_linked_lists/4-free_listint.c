8dc1fc72565215255f4225dc9236abe20a83da78
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-02-06 12:57:34 -0800

    fifth task

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint - a listint_t list.
+ * @head: Pointer to the list.
+ * Return: Nothing.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	temp = NULL;
+	if (head == NULL)
+	{
+		return;
+	}
+	else
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
