37f84dae42e7f4d1f2037219e29753b4dc890558
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-02-07 14:00:05 -0800

    first advanced task and correction on the tenth mandatory task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			nav = nav->next;
 		}
 		new->next = nav->next;
-		nav->next = new;
+		nav = new;
 	}
 	return (new);
 }
