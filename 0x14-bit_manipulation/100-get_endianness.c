821efb3e2395a84ff9fd785fcca5936edd991fa2
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-02-09 12:37:42 -0800

    first advanced task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_endianness --Checks for big and little endians.
+ * Return: Nothing.
+ */
+int get_endianness(void)
+{
+	if (0)
+	{
+		return (0);
+	}
+	else
+		return (1);
+}
