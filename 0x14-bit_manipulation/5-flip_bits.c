a2cfa62d8a74544836ea8297be83ad53beb3f5a1
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-02-08 17:37:53 -0800

    sixth mandatory task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * flip_bits --Number of bits to be flipped...
+ * @n: Fxn parameter.
+ * @m: Fxn parameter.
+ * Return: Integer.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bitMask = 1;
+	unsigned int i, num;
+
+	i = n ^ m;
+	num = 0;
+	while (i != 0)
+	{
+		if (i & bitMask)
+		{
+			num++;
+		}
+		i >>= 1;
+	}
+	return (num);
+}
