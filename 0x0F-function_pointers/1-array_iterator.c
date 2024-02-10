cb96273a2836cb0f2cfe249a81ad3aff3ccef1e3
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-02-10 07:19:21 -0800

    correct of task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int x;
 
-	x = 0;
-	for (x = 0; x < size; x++)
+	if (array != NULL && action != NULL)
 	{
-		action(array[x]);
+		x = 0;
+		for (x = 0; x < size; x++)
+		{
+			action(array[x]);
+		}
 	}
 }
