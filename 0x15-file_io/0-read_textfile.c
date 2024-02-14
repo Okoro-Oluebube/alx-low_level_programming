568c5314013449b2ed662757714b3897867fafd4
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-02-14 04:45:09 -0800

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * read_textfile --Reads a file.
+ * @filename: Pointer to the file to be read.
+ * @letters: No of bytes to be read.
+ * Return: Int.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t i, j;
+	int fd;
+	char buff[1024];
+
+	fd = open(filename, O_RDWR);
+	if (fd == -1 || filename == NULL)
+	{
+		return (0);
+	}
+	i = read(fd, buff, letters);
+	j = write(1, buff, i);
+	if (j < 0 || j != i)
+	{
+		return (0);
+	}
+	return (i);
+}
