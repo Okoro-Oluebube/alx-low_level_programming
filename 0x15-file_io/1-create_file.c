a620f6df2709ec62532c6d10a3a6d4633f0366a7
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-02-15 10:50:35 -0800

    task 1 mod. ii

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,9 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, nwr;
+	FILE *file;
 
+	file = fopen(filename, "r");
 	if (filename == NULL)
 	{
 		return (-1);
@@ -22,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	}
-	if (!filename)
+	if (file == NULL)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 		nwr = write(fd, text_content, strlen(text_content));
@@ -31,5 +33,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	fclose(file);
+	close(fd);
 	return (1);
 }
