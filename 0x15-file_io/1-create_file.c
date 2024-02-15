b00271474f4964f9d4dc2d478b8acd9ac0441f17
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-02-15 10:05:37 -0800

    task 1 mod

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,15 +13,23 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	else if (filename)
+	{
+		fd = open(filename, O_WRONLY);
+		nwr = write(fd, text_content, strlen(text_content));
+	}
 	else if (text_content == NULL)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY, 600);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY, 600);
-	nwr = write(fd, text_content, strlen(text_content));
-	return (1);
+	if (!filename)
+	{
+		fd = open(filename, O_CREAT | O_WRONLY, 600);
+		nwr = write(fd, text_content, strlen(text_content));
+	}
 	if (fd == -1 || nwr == -1)
 	{
 		return (-1);
 	}
+	return (1);
 }
