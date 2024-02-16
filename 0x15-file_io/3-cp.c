04b080f961482a5d22115d4a7d4a65cc97802859
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-02-16 14:05:47 -0800

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,5 +5,41 @@
  * @argc: Input parameter.
  * Return: Nothing.
  */
-int main (argc, argv)
+int main (int argc, char *argv[])
 {
+	int file_from, file_to, nr, nc;
+	char *buff[1024];
+
+	if (argc != 3)
+	{
+		dprintf(2, "%s\n", "Usage: cp file_from file_to");
+		exit (97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	if (file_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit (98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit (99);
+	}
+	nr = read(file_from, buff, 1024);
+	write(file_to, buff, nr);
+	nc = close(file_from);
+	if (nc == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", file_from);
+		exit (100);
+	}
+	nc = close(file_to);
+	if (nc == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", file_to);
+		exit (100);
+	}
+	return (0);
+}
