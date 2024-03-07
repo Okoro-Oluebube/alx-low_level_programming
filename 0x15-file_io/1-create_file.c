64481d5390ed36508accd562e931e91b66126c44
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-03-07 05:58:08 -0800

    correction on task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,14 +8,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, nwr;
-	FILE *file;
 
-	file = fopen(filename, "r");
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	if (file != NULL)
+	fd = open(filename, 0_RDONLY)
+	if (fd != NULL)
 	{
 		fd = open(filename, O_WRONLY);
 		nwr = write(fd, text_content, strlen(text_content));
@@ -24,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	}
-	if (file == NULL)
+	if (fd == NULL)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 		nwr = write(fd, text_content, strlen(text_content));
@@ -33,7 +32,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fclose(file);
 	close(fd);
 	return (1);
 }
