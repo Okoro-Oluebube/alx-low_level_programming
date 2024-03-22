0d8c6c82c4d016ae18970c5aa119c2e016f985a5
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-03-22 05:16:02 -0700

    mod on task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,18 +13,43 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int count;
 
 	temp1 = *h;
-	count = 0;
-	while (count < idx)
+	while (temp1)
 	{
 		count++;
 		temp1 = temp1->next;
 	}
-	temp = temp1->prev;
 	new = malloc(sizeof(dlistint_t));
-	temp->next = new;
-	new->prev = temp;
-	new->next = temp1;
-	temp1->prev = new;
-	new->n = n;
+	if (new == NULL)
+		return (NULL);
+	if (idx < count)
+	{
+		temp1 = *h;
+		count = 0;
+		while (count < idx)
+		{
+			count++;
+			temp1 = temp1->next;
+		}
+		temp = temp1->prev;
+		temp->next = new;
+		new->prev = temp;
+		new->next = temp1;
+		temp1->prev = new;
+		new->n = n;
+	}
+	else if (idx == count)
+	{
+		temp1 = *h;
+		while (temp1->next != NULL)
+		{
+			temp1 = temp1->next;
+		}
+		temp1->next = new;
+		new->prev = temp1;
+		new->next = NULL;
+		new->n = n;
+	}
+	else
+		return (NULL);
 	return (*h);
 }
